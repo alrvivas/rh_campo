068d05ea4bc08a6e407c2d6224c310cc5942f85e
alrvivas <alr.vivas@gmail.com>
2016-02-06 12:49:35 -0600

    v0.1 login

--- empleados/views.py
+++ empleados/views.py
@@ -16,6 +16,32 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.db.models import Q  
 
+def LoginView(request):
+    if not request.user.is_anonymous():
+        return redirect('index')
+    if request.POST:
+        formulario = AuthenticationForm(request.POST)
+        if formulario.is_valid:
+            usuario = request.POST['username']
+            clave = request.POST['password']
+            acceso = authenticate(username=usuario, password=clave)
+            if acceso is not None:
+                if acceso.is_active:
+                    login(request, acceso)
+                    return redirect('index')
+                else:
+                    return render_to_response('inactivousuario.html', context_instance=RequestContext(request))#no activo
+            else:
+                return render_to_response('nousuario.html', context_instance=RequestContext(request))#no usuario
+    else:
+        formulario = AuthenticationForm()
+    page_title = "Ingreso de Usuarios"
+    return render_to_response('login.html',{'formulario':formulario,'page_title':page_title}, context_instance=RequestContext(request))
+
+def LogoutView(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
 
 @login_required(login_url='/login/')
 def index(request):
